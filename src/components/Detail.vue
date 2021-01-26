3fba129e8ab797e80e926b0b1ccc656f0c380bdb
mjbc007 <mjbc007@gmail.com>
2021-01-26 15:19:31 -0600

    Primer commit

---
+++ src/components/Detail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <b-card :title="element.title">
+            <b-card-text>{{element.description}}</b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    created() {
+        this.getAll()
+    },
+    data() {
+        return {
+            elements: [],
+            msj: '',
+        }
+    },
+    methods: {
+        getAll() {
+            fetch('http://127.0.0.1:8000/api/element/?format=json')
+            .then(res => res.json())
+            .then(res => this.elements = res)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <b-card :title="element.title" v-for="(element) in elements" :key="element.id">
+            <b-card-text>{{element.description}}</b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    created() {
+        this.getAll()
+    },
+    data() {
+        return {
+            elements: [],
+        }
+    },
+    methods: {
+        getAll() {
+            fetch('http://127.0.0.1:8000/api/element/?format=json')
+            .then(res => res.json())
+            .then(res => this.elements = res)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
